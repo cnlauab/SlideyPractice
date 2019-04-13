7340f4d7e820771e9422e9d8a57393ec9ce92479
Francis Lau <francis3245000@hotmail.com>
2019-04-14 03:29:35 +0800

    first commit

---
+++ Assets/Box/Box.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour {
+
+    [SerializeField] public int size = 1;
+	// Use this for initialization
+	void Start () {
+        transform.localScale = new Vector3(size, 1, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+    }
+}
---
+++ Assets/Scripts/PlaySpace.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySpace : MonoBehaviour {
+
+    [SerializeField] public int height = 10;
+    [SerializeField] public int width = 8;
+    [SerializeField] public int score = 0;
+    [SerializeField] public int combo = 0;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void GameLoop(){
+        bool nextTurn = false;
+        SpawnLine();
+        PlayerTurn();
+        checkFall();
+        checkScore();
+    }
+
+    void PlayerTurn(){ 
+
+    }
+
+    void checkFall(){
+        for(each line){
+            for(each box){
+                if(can fall){
+                    fall();
+                }
+            }
+        }
+    }
+
+    bool checkScore(){
+        for(eachLine){
+        int count = 0;
+            for(each box){
+            count += box.size;
+            }
+            if(count == width){
+                score++;
+                combo++;
+            }
+        }
+    }
+
+    void SpawnLine(){
+
+    }
+
+}
